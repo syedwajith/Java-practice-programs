35ef8e0073b64a14633f210609fff387cdcb9dd6
syedwajith <syedwajith768@gmail.com>
2023-08-19 14:38:24 +0530

    second commit

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 10;
+        b = 10;
+        c = a + b;
+        System.out.println(c);
+    }
+}
---
+++ Division.java
@@ -0,0 +1,9 @@
+public class Division {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 10;
+        b = 3;
+        c = a / b;
+        System.out.println(c);
+    }
+}
